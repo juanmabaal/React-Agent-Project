9cd4c6ae713296f96df8640e4d88fa2bbe9b2eb7
Juan Manuel Balaguera <juanma.baal@gmail.com>
2026-05-25 17:44:58 -0500

    graph

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph, END
+from nodes import run_agent_reasoning, tool_node
 import os
 
 load_dotenv()
 
-def main():
-    print("Hello from langgraph-project!")
 
+AGENT_REASON='agent_reason'
+ACT='act'
+LAST=-1
+
+def should_continue(state: MessagesState) -> str:
+    if not state['messages'][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END: END,
+    ACT: ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 if __name__ == "__main__":
-    main()
-    print(os.getenv('DEEPSEEK_API_KEY'))
+    print("Hello, React Agent!")
+    res = app.invoke({"messages": [HumanMessage(content="What is the temperature in Tokyo? List it and then triple it")]})
+    print(res["messages"][LAST].content)
